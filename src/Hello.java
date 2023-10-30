2b2a688d14a6c00c938aece4ef94355de7626eaf
lkulig-wsb <lkulig@student.wsb-nlu.edu.pl>
2023-10-30 10:53:29 +0100

    Info o studencie v.1

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,15 @@
 public class Hello {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
+        //Wyswietlenie podstawowych info. o studencie za pomocą metody System.out.println
+        System.out.println("\n=== PODSTAWOWE INFO O STUDENCIE ===");
+        System.out.println("-------------------------------------");
+        System.out.println("Imię: Dave" );
+        System.out.println("Nazwisko: Grohl" );
+        System.out.println("Uczelnia: WSB-NLU" );
+        System.out.println("Kierunek: Informatyka" );
+        System.out.println("Rok: 2" );
+        System.out.println("Grupa: L" );
 
 
     }
