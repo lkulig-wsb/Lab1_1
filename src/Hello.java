349727a195e956a885345f07d04b42c464382d18
lkulig-wsb <lkulig@student.wsb-nlu.edu.pl>
2023-10-30 11:06:54 +0100

    Info o studencie v.2

--- src/Hello.java
+++ src/Hello.java
@@ -1,15 +1,23 @@
 public class Hello {
     public static void main(String[] args) {
 
+        //Utworzenie zmiennych i ich inicjalizacja danymi strudenta.
+        String imie = "Chris";
+        String nazwisko = "Cornell";
+        String uczelnia = "WSB-NLU";
+        String kierunek = "Informatyka";
+        int rok = 2;
+        char grupa = 'L';
+
         //Wyswietlenie podstawowych info. o studencie za pomocą metody System.out.println
         System.out.println("\n=== PODSTAWOWE INFO O STUDENCIE ===");
         System.out.println("-------------------------------------");
-        System.out.println("Imię: Dave" );
-        System.out.println("Nazwisko: Grohl" );
-        System.out.println("Uczelnia: WSB-NLU" );
-        System.out.println("Kierunek: Informatyka" );
-        System.out.println("Rok: 2" );
-        System.out.println("Grupa: L" );
+        System.out.println("Imię: " + imie);
+        System.out.println("Nazwisko: " + nazwisko);
+        System.out.println("Uczelnia: " + uczelnia);
+        System.out.println("Kierunek: " + kierunek);
+        System.out.println("Rok: " + rok);
+        System.out.println("Grupa: " + grupa);
 
 
     }
